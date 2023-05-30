f3ad2cfa2d6261958b4789ca93beb1db8176e7de
Rijad Husic <rijad.husic.93@gmail.com>
2023-05-30 22:20:01 +0200

    Header and Footer includes

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -10,6 +10,7 @@ class Pages extends Controller {
     }
 
     public function about() {
-        $this->view('pages/about', []);
+        $data = ['title' => 'About Us'];
+        $this->view('pages/about', $data);
     }
 }
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,1 +1,4 @@
-<h1><?= $data['title']; ?></h1>
+<?php require APP_ROOT . '/views/inc/header.php'; ?>
+    <h1><?= $data['title']; ?></h1>
+<?php require APP_ROOT . '/views/inc/footer.php'; ?>
+
