5d5bbbb01a855a4591defdf03d4258c87f24d0e7
Rijad Husic <rijad.husic.93@gmail.com>
2023-05-30 22:45:20 +0200

    Removed config from git, added an example config with sample data

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,27 @@
 require_once 'config/config.php';
 
 // Autoload Core Libraries
+// spl_autoload_register(function($className) {
+//     require_once 'libraries/' . $className . '.php';
+// });
+
 spl_autoload_register(function($className) {
-    require_once 'libraries/' . $className . '.php';
+    // Convert namespace separators to directory separators
+    $classFile = str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+    
+    // Specify the directories where your classes are located
+    $directories = [
+        APP_ROOT . '/controllers/',
+        APP_ROOT . '/models/',
+        APP_ROOT . '/libraries/'
+    ];
+
+    // Loop through the directories and check if the class file exists
+    foreach ($directories as $directory) {
+        $path = $directory . $classFile;
+        if (file_exists($path)) {
+            require_once $path;
+            return;
+        }
+    }
 });
